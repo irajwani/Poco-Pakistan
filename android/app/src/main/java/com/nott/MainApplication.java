f73b924d65ab3de29b685d8d55b41cad8985d53a
Imad Rajwani <imadrajwani@gmail.com>
2019-03-30 13:37:01 +0500

    PRODUCTION. version code 6, version 1. Did NOT generate correct release apk.

--- android/app/src/main/java/com/nott/MainApplication.java
+++ android/app/src/main/java/com/nott/MainApplication.java
@@ -48,7 +48,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ImageResizerPackage(),
           new RNGoogleSigninPackage(),
           new FBSDKPackage(mCallbackManager),
           new LinearGradientPackage(),
@@ -56,7 +55,8 @@ public class MainApplication extends Application implements ReactApplication {
           new RNFetchBlobPackage(),
           new SvgPackage(),
           new ReactNativePushNotificationPackage(),
-          new RNSpinkitPackage()
+          new RNSpinkitPackage(),
+          new ImageResizerPackage()
       );
     }
 
--- src/components/MultipleAddButton.js
+++ src/components/MultipleAddButton.js
@@ -39,13 +39,14 @@ class MultipleAddButton extends Component {
 
   launchCamera(navToComponent) {
     // console.log('launching camera');
-    if(Platform.OS == 'ios') {
-      this.props.navigation.navigate('MultiplePictureCamera', {navToComponent: `${navToComponent}` });
-    }
+    this.props.navigation.navigate('MultiplePictureCamera', {navToComponent: `${navToComponent}` });
+    // if(Platform.OS == 'ios') {
+    //   this.props.navigation.navigate('MultiplePictureCamera', {navToComponent: `${navToComponent}` });
+    // }
 
-    else {
-      alert('Sorry. We are currently working on this feature for Android phones. Please select picture from photo gallery for now.');
-    }
+    // else {
+    //   alert('Sorry. We are currently working on this feature for Android phones. Please select picture from photo gallery for now.');
+    // }
     
     //<MyCustomCamera />
     
